9e8157dc1805dfddeafa9819fc3757bbc52ac0be
Meeta <meetagriglani@gmail.com>
2017-08-15 19:28:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,29 @@ button.onclick = function() {
   request.onreadystatechange = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
           if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+
+          }
+      }
+  };
+  //Make a request
+  request.open('GET', 'http://meetagriglani.imad.hasura-app.io/counter', true);
+  request.send(null);
+};
+
+//Submit name
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    
+  //Create a request object
+  var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
               //Capture a list of names and render it as a list
               var names = request.responseText;
               names = JSON.parse(names);
@@ -24,14 +47,8 @@ button.onclick = function() {
       }
   };
   //Make a request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET', 'http://meetagriglani.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
-};
-
-//Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-  //Make a request to the server and send the name
 };
